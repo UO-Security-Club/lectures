303b505009103da58d14e5f3ba0af18943a628e8
John Brodnax <Brodnaxxx@128-223-223-65-wireless.uoregon.edu>
2016-10-04 18:27:04 -0700

    Aodh;f

--- week2/nameChange.sh
+++ week2/nameChange.sh
@@ -4,9 +4,10 @@
 
 for i in {1..100}
 do
-	python -c "print 'A'*$i + 'Hill'" | ./nameOverflow > output.txt
+	python -c "print 'A'*$i" | ./nameOverflow > output.txt
 
 	NAME=`grep Hill output.txt`
+	SEGF=`grep segfault output.txt`
 
 	if [ -z "$NAME" ]
 	then
@@ -15,5 +16,10 @@ do
 		echo "Input size is: $i"
 		echo $NAME
 		exit 1
+	fi
+
+	if [ -z "$SEGF" ]
+	then
+		echo "Segfault found, Size is: $i"
 	fi
 done
